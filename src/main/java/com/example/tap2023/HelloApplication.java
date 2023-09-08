5042be6c113c244dc826fa8cd4bc275cc308ffac
Ruben Torres Frias <ruben@itnora.com>
2023-09-07 18:33:50 -0600

    Clase 9

--- src/main/java/com/example/tap2023/HelloApplication.java
+++ src/main/java/com/example/tap2023/HelloApplication.java
@@ -4,21 +4,20 @@ import com.example.tap2023.vistas.Calculadora;
 import com.example.tap2023.vistas.Loteria;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloApplication extends Application {
 
     private Scene escena;
     private BorderPane borderPane;
     private MenuBar menuBar;
-    private Menu menuParcial1, menuParcial2;
-    private MenuItem mitCalculadora, mitLoteria;
+    private Menu menuParcial1, menuParcial2, menuSalir;
+    private MenuItem mitCalculadora, mitLoteria, mitSalir;
 
     private void CrearUI(){
         mitCalculadora = new MenuItem("Calculadora");
@@ -30,8 +29,25 @@ public class HelloApplication extends Application {
         menuParcial1.getItems().addAll(mitCalculadora,mitLoteria);
 
         menuParcial2 = new Menu("Parcial 2");
-        menuBar = new MenuBar(menuParcial1,menuParcial2);
+
+        menuSalir = new Menu("Mas opciones");
+        mitSalir = new MenuItem("Salir");
+        mitSalir.setOnAction((event)->Salir());
+        menuSalir.getItems().add(mitSalir);
+
+        menuBar = new MenuBar(menuParcial1,menuParcial2,menuSalir);
     }
+
+    private void Salir() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Mensaje del sistema");
+        alert.setHeaderText("¿Confirmar cerrar sistema?");
+        Optional<ButtonType> option =  alert.showAndWait();
+        if( option.get() == ButtonType.OK ){
+            System.exit(0);
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         CrearUI();
